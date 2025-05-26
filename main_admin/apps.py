a37b9a362c8fb5e5413c56f3e8f94a46233deecb
mevadasmit <mevadasmit2002@gmail.com>
2025-05-26 11:39:45 +0530

    feat :- Implemented the periodic task in apps.py

--- main_admin/apps.py
+++ main_admin/apps.py
@@ -1,6 +1,24 @@
 from django.apps import AppConfig
 
-
 class AdminConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "main_admin"
+
+    def ready(self):
+        try:
+            from main_admin.tasks import export_org_csv
+            from django_celery_beat.models import PeriodicTask , IntervalSchedule
+            from django.db.utils import OperationalError , ProgrammingError
+
+            schedule, _ = IntervalSchedule.objects.get_or_create(
+                every=2,
+                period=IntervalSchedule.MINUTES,
+            )
+
+            periodic_task = PeriodicTask.objects.get_or_create(
+                interval=schedule,
+                name="Export Org CSV Every 2 Minutes",
+                task="main_admin.tasks.export_org_csv",
+            )
+        except (OperationalError, ProgrammingError) as e:
+            pass
